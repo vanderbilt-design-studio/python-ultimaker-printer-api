ab385fbcb197f8bf3edb1e55d06ffe55e35f0680
Sameer Puri <sameer@users.noreply.github.com>
2019-02-14 23:47:36 -0600

    Initial commit with updated API

---
+++ test/test_ultimaker.py
@@ -0,0 +1,232 @@
+import unittest
+from unittest.mock import Mock, patch
+import json
+from ultimaker import Printer, CredentialsDict, Credentials, Identity
+from uuid import UUID, uuid4
+import os
+from datetime import timedelta
+
+mock_identity = Identity('mock application', 'mock user')
+mock_name = '2D Printer'
+mock_guid: UUID = uuid4()
+mock_address = '127.0.0.1'
+mock_port = '8080'
+mock_id = '1234'
+mock_key = 'abcd'
+mock_credentials = Credentials(mock_id, mock_key)
+mock_credentials_json = mock_credentials._asdict()
+mock_print_job_time_elapsed = timedelta(seconds=30)
+mock_print_job_time_total = timedelta(seconds=60)
+mock_print_job_progress = 30./60.
+mock_print_job_state = 'printing'
+mock_camera_snapshot_uri = 'data:image/png,base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAMAAAAoLQ9TAAAABGdBTUEAALGPC/xhBQAAACBjSFJN\
+AAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAC01BMVEUAAAA4Oz0rLzVLTUhV\
+V04tMTYuMjckKTFNT0k9QED//8qGhWxdXlOMi295eWT//9CHQUgqLzQzNjpMMTqtMkUoMTUkMTUg\
+MTQvMTZbXVKWlHW3tYrDwZG8uo2gn3xtbl0AAAoAAAB8e2XEwJHPy5iQj3IoLDN9fGbGwpKSkHMA\
+AAFGSUW8uIzLxpVzc2BMTkmUknSnpYBfYFQAAACWlHWwrYUAAACfnXuCgmqQhW25tYp1dmJ7PUZZ\
+QUZhPkVYTk9OSkxaTk9YX1tlaF51dWFCMzlbNj+Eem9nZF1ua2NnWldITU00NzosMDU5NTpiXlmM\
+g3ZramR1c2pqaGJmYVssMDZDRESFfHCSh3h7dWttbGW4p5F6d214dGp/eG2dkYF+eG1pY1wFDxxt\
+Z2BcXFh3c2p+eW+GfHGJfHFzamSFgHNtamJfW1ZERUUZHylWVVGxoo15ZF8uKjIAHyUoMTVDMjmH\
+WlqNM0JCMThWMTueMUKrMkWXMkNrMj0AMC4AMC5fMjyDMkBsOUKHc2qQhHZXQkVHLzghMTUALAPR\
+y5i4nX26lnq3lnrGvI/t5qq9tImOZFuMQUmIVFKqmXnl36XX0pzd16DZ053GvY3e2KCWX1rIuI7U\
+y5fLxZTh26PMx5a+uIuxqoK0qoDXzJiOSE26ooDMwpCjnHjBuozLxpXo4qfa053QypfEv5ChmXau\
+pX6JcmGhlHSooHqzroTY0p3Nx5Xw6avZ1Z63sIeyn36klHeMhWyNfmmKfmmpkXWpn32uq4TUz5tz\
+Z1p1OUJQOT+GN0SJOkZyV1aPgXWdkH+Vdm6GenCVi3x5dWyCfnLHs5qAeG58dm2Lg3ZrZ2KekoFy\
+cGixoYxycGdqaWK3pY9+eW/Jt5yhcGulPUuWO0iiOEemQE2ke3Kyl4aiV1u3NUjOMknGMkjFMke+\
+Mke1NkjHMUfJM0nEM0iaQU2MTVKxNEf///+IU9e3AAAAiXRSTlMAAAAAAAAAAAAAAAAAAAAAAAAA\
+AAAAAAAAPqrq/fLAWgcDaub0khBf9JAFItn0SxSRuy0CleEFsP7+/YD+/v7+/v7+/s9r9v7+/v7j\
+RDaR6P7+/vRaKybd/v7+/v7+/v74UQWP5v7+/v7+/uyeGxVn9txiAiCj7/BAYPPz1XUEBWvJ2/ju\
+sE0aAWTj0qsAAAABYktHRPA1uO9UAAAAB3RJTUUH4gwEEAYJ7mrWLQAAARtJREFUGNMBEAHv/gAA\
+AAECABkaGxwdHh8gAwQFAAAGByEiI4mKi4yNJCUmCAkAAAAKJyiOj5CRkpOUlSkqAgALDCsslpeY\
+mZqbnJ2eLS4NAA4vMJ+goaKjpKWmp6ipMTIAMzSqq6ytrq+wsbKztLW2NQA2N7e4ubq7vL04Ob6/\
+wME6AA87wsM8PcQ+P0DFxkFCQ0QAEEVGx0dIyMlJSsrLzM1LTABNTk/Oz9DRUFFS0tPP1FNUAFVW\
+V9XWWFlaW1xdXl/XYGEABWJjZNhlZmfZaGlq2mtsbQAREm5vcNvc3d7f4OHicXJzAAUFdHV24+Tl\
+5ufl6Ol3eBMAAAUUeXrq6+zt7u97fH1+FQAABRZ/gIGBgoOEhYaHiBcYvRtyWUVkU38AAAAldEVY\
+dGRhdGU6Y3JlYXRlADIwMTgtMTItMDRUMjI6MDY6MDktMDY6MDDsXvBMAAAAJXRFWHRkYXRlOm1v\
+ZGlmeQAyMDE4LTEyLTA0VDIyOjA2OjA5LTA2OjAwnQNI8AAAAABJRU5ErkJggg=='
+
+
+def default_printer_mock() -> Printer:
+    printer = Printer(mock_address, mock_port, mock_identity, mock_credentials)
+    # TODO: understand unittest.mock patch method so the set_credentials method can be asserted on
+    # printer.set_credentials = patch.object(printer, 'set_credentials', wraps=printer.set_credentials)
+    printer.post_auth_request = Mock(
+        return_value=mock_credentials_json)
+    printer.get_auth_check = Mock(return_value='authorized')
+    printer.get_auth_verify = Mock(return_value=True)
+    printer.get_system_name = Mock(return_value=mock_name)
+    printer.get_printer_status = Mock(return_value='idle')
+    return printer
+
+
+def default_credentials_dict_mock() -> CredentialsDict:
+    credentials_dict = CredentialsDict('/tmp/credentials.json')
+    credentials_dict[mock_guid] = Credentials(**mock_credentials_json)
+    credentials_dict.save = Mock()
+    return credentials_dict
+
+
+class AcquireCredentialsTest(unittest.TestCase):
+    def setUp(self):
+        printer = default_printer_mock()
+        printer.credentials = None
+        self.printer = printer
+
+    def test_printer_acquires_credentials(self):
+        self.printer.get_credentials()
+        self.assertTrue(self.printer.credentials is not None)
+        self.printer.post_auth_request.assert_called_once()
+        self.printer.get_auth_check.assert_not_called()
+        self.printer.get_auth_verify.assert_not_called()
+
+    def test_printer_acquires_credentials_only_once(self):
+        self.printer.get_credentials()
+        self.assertTrue(self.printer.credentials is not None)
+        self.printer.post_auth_request.assert_called_once()
+        self.printer.get_auth_check.assert_not_called()
+        self.printer.get_auth_verify.assert_not_called()
+
+
+class AlreadyHasCredentialsTest(unittest.TestCase):
+    def setUp(self):
+        printer = default_printer_mock()
+        printer.credentials = Mock(return_value=mock_credentials)
+        self.printer = printer
+
+    def test_printer_is_authorized(self):
+        self.assertTrue(self.printer.is_authorized())
+        self.printer.credentials.assert_called_once()
+        self.printer.post_auth_request.assert_not_called()
+        self.printer.get_auth_check.assert_called_once()
+
+
+class UltimakerJsonTest(unittest.TestCase):
+    def test_expected_json_is_produced_when_idle(self):
+        printer = default_printer_mock()
+        printer.credentials = Mock(return_value=mock_credentials)
+        printer.get_camera_snapshot_uri = Mock(
+            return_value=mock_camera_snapshot_uri
+        )
+        json = printer.into_ultimaker_json()
+        self.assertDictEqual({
+            'system': {
+                'name': mock_name
+            },
+            'printer': {
+                'status': 'idle'
+            },
+            'camera': {
+                'snapshot': mock_camera_snapshot_uri
+            }
+        }, json)
+
+    def test_expected_json_is_produced_when_errored(self):
+        printer = default_printer_mock()
+        printer.credentials = Mock(return_value=mock_credentials)
+        printer.get_printer_status = generic_exception_raiser
+        printer.get_print_job_state = Mock(return_value=mock_print_job_state)
+        json = printer.into_ultimaker_json()
+        self.assertDictEqual({'system': {
+            'name': mock_name
+        }}, json)
+
+    def test_expected_json_is_produced_when_printing(self):
+        printer = default_printer_mock()
+        printer.credentials = Mock(return_value=mock_credentials)
+        printer.get_printer_status = Mock(return_value='printing')
+        printer.get_print_job_time_elapsed = Mock(
+            return_value=mock_print_job_time_elapsed)
+        printer.get_print_job_time_total = Mock(
+            return_value=mock_print_job_time_total)
+        printer.get_print_job_progress = Mock(
+            return_value=mock_print_job_progress)
+        printer.get_print_job_state = Mock(
+            return_value=mock_print_job_state)
+        printer.get_camera_snapshot_uri = Mock(
+            return_value=mock_camera_snapshot_uri
+        )
+
+        json = printer.into_ultimaker_json()
+        self.assertDictEqual({
+            'system': {
+                'name': mock_name
+            },
+            'printer': {
+                'status': 'printing'
+            },
+            'print_job': {
+                'time_elapsed': str(mock_print_job_time_elapsed),
+                'time_total': str(mock_print_job_time_total),
+                'progress': mock_print_job_progress,
+                'state': mock_print_job_state
+            },
+            'camera': {
+                'snapshot': mock_camera_snapshot_uri
+            }
+        }, json)
+
+
+def generic_exception_raiser():
+    raise Exception('An exception has occurred')
+
+
+class VerifiesLoadedCredentialsTest(unittest.TestCase):
+    def setUp(self):
+        printer = default_printer_mock()
+        self.printer = printer
+        self.printer.get_auth_verify = Mock(return_value=False)
+
+    def test_printer_is_not_verified(self):
+        self.assertFalse(self.printer.credentials is None)
+        self.printer.get_credentials()
+        self.assertTrue(self.printer.credentials is not None)
+        self.printer.post_auth_request.assert_called_once()
+        self.printer.get_auth_verify.assert_called_once()
+
+
+class CredentialsDictTest(unittest.TestCase):
+    def setUp(self):
+        self.credentials_dict_json = {mock_guid.hex: mock_credentials_json}
+        random_filename = f'/tmp/credentials_{uuid4()}.json'
+        with open(random_filename, 'w+') as credentials_file:
+            json.dump(self.credentials_dict_json, credentials_file)
+        self.credentials_dict = CredentialsDict(random_filename)
+
+    def test_credentials_file_loads_correctly(self):
+        self.assertTrue(mock_guid in self.credentials_dict)
+        self.assertDictEqual(mock_credentials_json,
+                             self.credentials_dict[mock_guid]._asdict())
+
+    def test_credentials_file_saves_correctly(self):
+        self.credentials_dict.save()
+        with open(self.credentials_dict.credentials_filename, 'r') as credentials_file:
+            saved_json = json.load(credentials_file)
+        self.assertDictEqual(self.credentials_dict_json, saved_json)
+
+    def tearDown(self):
+        os.remove(self.credentials_dict.credentials_filename)
+
+
+class CredentialsDictEdgeCaseTest(unittest.TestCase):
+    def test_credentials_file_loads_empty_when_json_completely_invalid(self):
+        invalid_json_credentials_dict = CredentialsDict(
+            f'/tmp/credentials_{uuid4()}.json')
+        self.assertDictEqual(invalid_json_credentials_dict, {})
+        os.remove(invalid_json_credentials_dict.credentials_filename)
+
+    def test_credentials_file_loads_some_when_json_partially_invalid(self):
+        partially_valid_json_filename = f'/tmp/credentials_{uuid4()}.json'
+        with open(partially_valid_json_filename, 'w') as credentials_file:
+            json.dump({mock_guid.hex: mock_credentials_json,
+                       uuid4().hex: 'invalid'}, credentials_file)
+        partially_valid_json_credentials_dict = CredentialsDict(
+            partially_valid_json_filename)
+        self.assertTrue(
+            mock_guid in partially_valid_json_credentials_dict)
+        self.assertDictEqual(
+            mock_credentials_json, partially_valid_json_credentials_dict[mock_guid]._asdict())
+        os.remove(partially_valid_json_credentials_dict.credentials_filename)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ ultimaker.py
@@ -0,0 +1,217 @@
+from collections import namedtuple, OrderedDict
+from typing import Dict
+from zeroconf import ServiceInfo
+import requests
+from requests.auth import HTTPDigestAuth
+from config import ultimaker_application_name, ultimaker_user_name, ultimaker_credentials_filename
+import json
+from uuid import UUID
+import re
+from datetime import timedelta
+import base64
+
+# The mDNS response looks like this:
+#   ServiceInfo(
+#       type='_printer._tcp.local.',
+#       name='ultimakersystem-REDACTED._printer._tcp.local.',
+#       address=b'\xc0\xa8\x01\x12',
+#       port=80,
+#       weight=0,
+#       priority=0,
+#       server='ultimakersystem-REDACTED.local.',
+#       properties={
+#           b'type': b'printer',
+#           b'hotend_serial_0': b'REDACTED',
+#           b'cluster_size': b'1',
+#           b'firmware_version': b'4.3.3.20180529',
+#           b'machine': b'9066.0',
+#           b'name': b'U1',
+#           b'hotend_type_0': b'AA 0.4'
+#       }
+#   )
+
+# A user/password pair
+Credentials = namedtuple('Credentials', ['id', 'key'])
+
+# An application/user name pair displayed on the printer when requesting authorization
+Identity = namedtuple('Name', ['application_name', 'user_name'])
+
+
+class CredentialsDict(OrderedDict):
+    def __init__(self, credentials_filename):
+        self.credentials_filename = credentials_filename
+        with open(credentials_filename, 'a+') as credentials_file:
+            try:
+                credentials_file.seek(0)
+                credentials_json = json.load(credentials_file)
+            except Exception as e:
+                print(
+                    f'Exception in parsing credentials.json, pretending it is empty: {e}')
+                credentials_json = {}
+        guid: UUID
+        credentials: Credentials
+        for guid, credentials in credentials_json.items():
+            try:
+                # Convert json to a dictionary of field to value mappings
+                kwargs = dict([(field, credentials[field])
+                               for field in Credentials._fields])
+                self[UUID(guid)] = Credentials(**kwargs)
+            except Exception as e:
+                print(
+                    f'Exception in parsing the credentials instance in credentials.json with guid {guid}, skipping it: {e}')
+
+    def save(self):
+        credentials_json: Dict[str, str] = {}
+        guid: UUID
+        credentials: Credentials
+        for guid, credentials in self.items():
+            credentials_json[guid.hex] = credentials._asdict()
+        with open(self.credentials_filename, 'w') as credentials_file:
+            json.dump(credentials_json, credentials_file)
+
+# {
+#   "time_elapsed": 0,
+#   "time_total": 0,
+#   "datetime_started": "2018-10-10T00:46:40.776Z",
+#   "datetime_finished": "2018-10-10T00:46:40.776Z",
+#   "datetime_cleaned": "2018-10-10T00:46:40.776Z",
+#   "source": "string",
+#   "source_user": "string",
+#   "source_application": "string",
+#   "name": "string",
+#   "uuid": "string",
+#   "reprint_original_uuid": "string",
+#   "state": "none"
+# }
+PrintJob = namedtuple('PrintJob', ['time_elapsed', 'time_total', 'datetime_started', 'datetime_finished',
+                                   'datetime_cleaned', 'source', 'source_user', 'source_application', 'name', 'uuid', 'reprint_original_uuid', 'state', 'progress'])
+
+
+class Printer():
+    def __init__(self, address: str, port: int, identity: Identity, credentials: Credentials = None):
+        self.address = address
+        self.host = f'{address}:{port}'
+        self.identity = identity
+        self.credentials = credentials
+        self.guid = None
+
+    def acquire_credentials(self):
+        credentials_json = self.post_auth_request()
+        self.set_credentials(credentials_json)
+
+    def get_credentials(self) -> Credentials:
+        if self.credentials is None:
+            self.acquire_credentials()
+        elif not self.get_auth_verify():
+            self.credentials = None
+            self.acquire_credentials()
+        return self.credentials
+
+    def set_credentials(self, credentials: Credentials):
+        self.credentials = credentials
+
+    def digest_auth(self) -> HTTPDigestAuth:
+        credentials = self.credentials()
+        return HTTPDigestAuth(credentials.id, credentials.key)
+
+    def is_authorized(self) -> bool:
+        self.credentials()
+        return self.get_auth_check() == 'authorized'
+
+    def into_ultimaker_json(self) -> Dict[str, str]:
+        try:
+            status = self.get_printer_status()
+            ultimaker_json = {
+                'system': {
+                    'name': self.get_system_name(),
+                },
+                'printer': {
+                    'status': status,
+                },
+                'camera': {
+                    'snapshot': self.get_camera_snapshot_uri()
+                }
+            }
+            if status == 'printing':
+                ultimaker_json['print_job'] = {
+                    'time_elapsed': str(self.get_print_job_time_elapsed()),
+                    'time_total': str(self.get_print_job_time_total()),
+                    'progress': self.get_print_job_progress(),
+                    'state': self.get_print_job_state(),
+                }
+                ultimaker_json['camera'] = {
+                    'snapshot': self.get_camera_snapshot_uri()
+                }
+            return ultimaker_json
+        except Exception as e:
+            print(f'Exception while generating ultimaker json {e}')
+            return {
+                'system': {
+                    'name': self.get_system_name(),
+                },
+            }
+
+    # All of the request functions below are from the Ultimaker Swagger Api available at http://PRINTER_ADDRESS/docs/api/
+    # You can usually only call things other than /auth/check and /auth/request when you have credentials. As far as I've
+    # tested, you don't need credentials for get queries. To be on the safe side, credentials are requested.
+    # -------------------------------------------------------------------------------------------------------------------
+
+    def post_auth_request(self) -> Dict:
+        return requests.post(url=f"http://{self.host}/api/v1/auth/request",
+                             data={'application': ultimaker_application_name, 'user': ultimaker_user_name}).json()
+
+    # Returns the response from an authorization check
+    def get_auth_check(self) -> str:
+        return requests.get(url=f"http://{self.host}/api/v1/auth/check/{self.credentials.id}").json()['message']
+
+    # Returns whether the credentials are known to the printer. They may not be if the printer was reset.
+    # Note that this is completely different from get_auth_check.
+    def get_auth_verify(self) -> bool:
+        return requests.get(
+            url=f"http://{self.host}/api/v1/auth/verify", auth=HTTPDigestAuth(self.credentials.id, self.credentials.key)).ok
+
+    def get_printer_status(self) -> str:
+        return requests.get(
+            url=f"http://{self.host}/api/v1/printer/status", auth=self.digest_auth()).json()
+
+    def get_print_job(self) -> PrintJob:
+        return PrintJob(**requests.get(
+            url=f"http://{self.host}/api/v1/print_job", auth=self.digest_auth()).json())
+
+    def get_print_job_state(self) -> str:
+        return requests.get(
+            url=f"http://{self.host}/api/v1/print_job/state", auth=self.digest_auth()).json()
+
+    def get_print_job_time_elapsed(self) -> timedelta:
+        return timedelta(seconds=requests.get(
+            url=f"http://{self.host}/api/v1/print_job/time_elapsed", auth=self.digest_auth()).json())
+
+    def get_print_job_time_total(self) -> timedelta:
+        return timedelta(seconds=requests.get(
+            url=f"http://{self.host}/api/v1/print_job/time_total", auth=self.digest_auth()).json())
+
+    def get_print_job_progress(self) -> float:
+        return requests.get(
+            url=f"http://{self.host}/api/v1/print_job/progress", auth=self.digest_auth()).json()
+
+    def get_print_job_name(self) -> str:
+        return requests.get(
+            url=f"http://{self.host}/api/v1/print_job/name", auth=self.digest_auth()).json()
+
+    def put_system_display_message(self, message: str, button_caption: str) -> str:
+        return requests.put(url=f"http://{self.host}/api/v1/system/display_message", auth=self.digest_auth(), json={'message': message, 'button_caption': button_caption}).json()
+
+    # Doesn't work as far as I've tested it
+    def put_beep(self, frequency: float, duration: float) -> str:
+        return requests.put(url=f"http://{self.host}/api/v1/beep", auth=self.digest_auth(), json={'frequency': frequency, 'duration': duration}).json()
+
+    def get_system_guid(self) -> UUID:
+        return UUID(requests.get(url=f'http://{self.host}/api/v1/system/guid').json())
+
+    def get_system_name(self) -> str:
+        return requests.get(url=f'http://{self.host}/api/v1/system/name').json()
+
+    def get_camera_snapshot_uri(self) -> str:
+        res: requests.Response = requests.get(
+            url=f'http://{self.address}:8080/?action=snapshot')
+        return f"data:{res.headers['Content-Type']};base64,{base64.b64encode(res.content).decode('utf-8')}"
